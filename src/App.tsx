fd134d09091b5f870debf7b866cd980a2220a446
unknown <pinedavictor095@gmail.com>
2022-02-03 23:25:26 -0800

    Added assets and re-structured folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
+// import { Logo } from "./assets/vectors";
+import "./styles/App.css";
+
+document.getElementById;
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="App bg-black">
       <header className="">
         <p>Dreamlike using VITE</p>
+        {/* <Logo width={"3rem"} height={"3rem"} /> */}
       </header>
     </div>
   );
---
+++ src/assets/vectors/index.tsx
@@ -0,0 +1,3 @@
+export { ReactComponent as DLDSymbol } from "./symbol.svg";
+export { ReactComponent as Logo } from "./DLD.svg";
+export { ReactComponent as MagicThree } from "./ThreeSquares.svg";
