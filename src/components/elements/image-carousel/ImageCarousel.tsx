b6812176014e7021822725e301c2576e6b6c4231
PinedaVictor <pinedavictor095@gmail.com>
2022-02-05 17:15:05 -0800

    Added ui element component headers, installed react spring and gestures

---
+++ src/components/elements/image-carousel/ImageCarousel.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import img from "../../../assets/images/idk.png";
+
+export const ImageCarousel: React.FC = () => {
+  return (
+    <>
+      <div className=" bg-offBlack bg-[url(`../../../assets/images/idk.png`)] text-yellow">
+        My image carousel
+      </div>
+    </>
+  );
+};
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-
+import { ImageCarousel } from "../elements/image-carousel/ImageCarousel";
 export const Hero: React.FC = () => {
   return (
     <>
       <section className="bg-tan h-screen top-0 flex">
         <div className="bg-yellow h-full  w-full">
-          <div className="  bg-offBlack inline-block">
-            <p>Some text that is catchy</p>
-          </div>
+          <ImageCarousel />
         </div>
       </section>
     </>
