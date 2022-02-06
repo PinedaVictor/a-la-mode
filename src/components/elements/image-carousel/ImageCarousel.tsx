2a4be2d77aeb9609ca00068d19a46a5488406f2e
unknown <pinedavictor095@gmail.com>
2022-02-06 00:59:09 -0800

    Added image state to carousel

--- src/components/elements/image-carousel/ImageCarousel.tsx
+++ src/components/elements/image-carousel/ImageCarousel.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import img from "../../../assets/images/idk.png";
 import img2 from "../../../assets/images/yes.jpg";
 
 const images = [img, img2];
 
 export const ImageCarousel: React.FC = () => {
+  const [currentImg, setCurrentImg] = useState(img);
+  // TODO: Get started with gestures API
+  // TODO: Associate gesture with moving right and left of an image
+
   return (
     <>
-      <div className="text-offBlack">My image carousel</div>
       <div className=" h-full w-full">
         <picture>
           <img
-            src={img}
+            src={currentImg}
             alt="Make sure alt text is in data"
             className="object-cover h-full w-full"
           />
