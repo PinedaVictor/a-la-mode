414426874094204f86392dfbb6a76728aeb7af5e
unknown <pinedavictor095@gmail.com>
2022-02-05 22:56:47 -0800

    Updated page title and added module decloration

--- src/components/elements/image-carousel/ImageCarousel.tsx
+++ src/components/elements/image-carousel/ImageCarousel.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import img from "../../../assets/images/idk.png";
+import img2 from "../../../assets/images/yes.jpg";
+
+const images = [img, img2];
 
 export const ImageCarousel: React.FC = () => {
   return (
     <>
       <div className="text-offBlack">My image carousel</div>
-      <img src={img} />
+      <div className=" h-full w-full">
+        <picture>
+          <img
+            src={img}
+            alt="Make sure alt text is in data"
+            className="object-cover h-full w-full"
+          />
+        </picture>
+      </div>
     </>
   );
 };
