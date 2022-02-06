b5e849fd9cee3eac00c994ca9601654df085043c
PinedaVictor <pinedavictor095@gmail.com>
2022-02-05 21:08:52 -0800

    Added node types for ts, custom types, and added image carousel component header

--- src/components/elements/image-carousel/ImageCarousel.tsx
+++ src/components/elements/image-carousel/ImageCarousel.tsx
@@ -4,9 +4,8 @@ import img from "../../../assets/images/idk.png";
 export const ImageCarousel: React.FC = () => {
   return (
     <>
-      <div className=" bg-offBlack bg-[url(`../../../assets/images/idk.png`)] text-yellow">
-        My image carousel
-      </div>
+      <div className="text-offBlack">My image carousel</div>
+      <img src={img} />
     </>
   );
 };
