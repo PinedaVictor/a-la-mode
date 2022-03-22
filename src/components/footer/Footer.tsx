eec6fb2c4b38111d96e7eb7d665950f2b3d52834
PinedaVictor <pinedavictor095@gmail.com>
2022-03-22 00:16:01 -0700

    Added footer copy and added site map

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,25 +1,40 @@
 import React from "react";
 import { FooterSection } from "./FooterSection";
 import { Pitch } from "./Pitch";
+import { Link } from "react-router-dom";
 
 export const Footer: React.FC = () => {
   return (
     <>
-      <div className="flex flex-wrap p-5 grid-cols-3 font-TY">
+      <div className="flex flex-wrap p-5 grid-cols-3 ">
         <FooterSection>
-          {/* <p>About me</p> */}
           <Pitch />
         </FooterSection>
         <FooterSection>
           Social Media links
+          {/* TODO: Get social links */}
           <ul>
             <li>Twitter</li>
             <li>Instagram</li>
             <li>LinkedIn</li>
+            <li>Github</li>
           </ul>
         </FooterSection>
         <FooterSection>
-          <p>Site map</p>
+          <ul>
+            <Link to="/">
+              <li className=" ">Home</li>
+            </Link>
+            <Link to="/WebApps">
+              <li className=" ">Web Apps</li>
+            </Link>
+            <Link to="/HappyClients">
+              <li className=" ">Clients</li>
+            </Link>
+            <Link to="/HappyClients">
+              <li className=" ">Pixels</li>
+            </Link>
+          </ul>
         </FooterSection>
         <div className="">&copy; Dreamlike Digital. All Rights Reserved</div>
       </div>
--- src/components/footer/Pitch.tsx
+++ src/components/footer/Pitch.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import img from "../../assets/images/art.jpg";
+import img from "../../assets/images/vicgithub.jpg";
 import { Avatar } from "../elements";
 
 export const Pitch: React.FC = () => {
   return (
     <>
       <Avatar img={img} imgAltText="Victor Pineda profile picture" />
-      <div>THe pith compoent</div>
+      <div>Hi, 👋</div>
+      <div>
+        My name is Victor Pineda. Here to help you with your brand, NFT
+        colleciton, and front-end development. I have 3 Years of industry
+        expeirence and a creative mind that works to help you reach your goals.
+        Feel free to reach at any of my socials or lets chat over email
+        pinedavictor095@gmail.com
+      </div>
     </>
   );
 };
