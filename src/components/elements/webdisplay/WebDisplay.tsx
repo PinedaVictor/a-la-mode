d47c448422d60721a60c6d1d238834abef274248
PinedaVictor <pinedavictor095@gmail.com>
2022-03-24 18:12:47 -0700

    Added media and fixed animation bug

--- src/components/elements/webdisplay/WebDisplay.tsx
+++ src/components/elements/webdisplay/WebDisplay.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ExternalLink } from "../../utils";
-import { LeftRightSpring } from "../../springs";
 
 interface WebDisplayProps {
   image: { src: string; altText: string };
@@ -11,31 +10,29 @@ interface WebDisplayProps {
 export const WebDisplay: React.FC<WebDisplayProps> = (props) => {
   return (
     <>
-      <LeftRightSpring left={false} height={500}>
-        <div className=" p-7">
-          <div className=" ">
-            <div className="">
-              <picture>
-                <img
-                  className=" rounded-t-lg"
-                  src={props.image.src}
-                  width={1080}
-                  height={1080}
-                  alt={props.image.altText}
-                />
-              </picture>
-            </div>
-            <div className="flex p-2 font-SFM border-x-2 border-b-2 rounded-b-lg border-grey">
-              <p className=" ">{props.title}</p>
-              <div className="w-full">
-                <ExternalLink link={props.link}>
-                  <div className=" float-right mr-3 mt-3 text-2xl ">🚀</div>
-                </ExternalLink>
-              </div>
+      <div className=" p-7">
+        <div className=" ">
+          <div className="">
+            <picture>
+              <img
+                className=" rounded-t-lg"
+                src={props.image.src}
+                width={1080}
+                height={1080}
+                alt={props.image.altText}
+              />
+            </picture>
+          </div>
+          <div className="flex p-2 font-SFM border-x-2 border-b-2 rounded-b-lg border-grey">
+            <p className=" ">{props.title}</p>
+            <div className="w-full">
+              <ExternalLink link={props.link}>
+                <div className=" float-right mr-3 mt-3 text-2xl ">🚀</div>
+              </ExternalLink>
             </div>
           </div>
         </div>
-      </LeftRightSpring>
+      </div>
     </>
   );
 };
--- src/pages/WebApps.tsx
+++ src/pages/WebApps.tsx
@@ -3,7 +3,9 @@ import { HeaderNav } from "../components";
 import { WebDisplay } from "../components/elements";
 import img from "../assets/images/webapps/UIS.jpg";
 import vicApp from "../assets/images/webapps/vicApp.jpg";
+import csunnft from "../assets/images/webapps/csunnft.jpg";
 import landing from "../assets/images/webapps/dldLanding.jpg";
+import { LeftRightSpring } from "../components/springs";
 
 const WebAppsData = [
   {
@@ -21,22 +23,29 @@ const WebAppsData = [
     title: "United International",
     link: "https://unitedinternationalservices.com/",
   },
+  {
+    image: { src: csunnft, altText: "United International web display" },
+    title: "CSUN NFT",
+    link: "https://thatnft.io/",
+  },
 ];
 
 export const WebApps: React.FC = () => {
   return (
     <>
       <HeaderNav />
-      <section className="grid md:grid-cols-3">
-        {WebAppsData.map((item, index) => (
-          <WebDisplay
-            key={index}
-            image={item.image}
-            link={item.link}
-            title={item.title}
-          />
-        ))}
-      </section>
+      <LeftRightSpring left={true} height={500}>
+        <section className="grid md:grid-cols-3">
+          {WebAppsData.map((item, index) => (
+            <WebDisplay
+              key={index}
+              image={item.image}
+              link={item.link}
+              title={item.title}
+            />
+          ))}
+        </section>
+      </LeftRightSpring>
     </>
   );
 };
