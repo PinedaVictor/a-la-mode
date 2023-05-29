32a26c2806b1dca903fefe1d234a703711d7325a
PinedaVictor <pinedavictor095@gmail.com>
2023-05-29 07:15:45 -0700

    Added form validation util and error handling

--- src/components/atomic/molecules/ContactForm.tsx
+++ src/components/atomic/molecules/ContactForm.tsx
@@ -2,23 +2,21 @@ import { Trails } from "../../springs/Trails";
 import { Input, TextArea } from "../atoms";
 import { Comment } from "../../elements/clients/Comment";
 import { CheckCircleIcon } from "@heroicons/react/20/solid";
-import { useForm } from "react-hook-form";
-
-type V = {
-  value: string;
-};
-
-interface Target extends EventTarget {
-  name: V;
-  email: V;
-  phoneNumber: V;
-  msg: V;
-}
+import { Target, validateContactForm } from "../../utils/validateContactForm";
+import { useState } from "react";
 
 export const ContactForm: React.FC = () => {
+  const [errMsg, setErrMsg] = useState("");
+
+  const handleErrMsg = (msg: string) => {
+    setErrMsg(msg);
+    setTimeout(() => {
+      setErrMsg("");
+    }, 3500);
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     const target = e.target as Target;
     const data = {
       name: target.name.value,
@@ -26,45 +24,52 @@ export const ContactForm: React.FC = () => {
       phoneNumber: target.phoneNumber.value,
       msg: target.msg.value,
     };
-    console.log("The data:", data);
+    const validInputs = validateContactForm(data);
+    if (!validInputs.valid) {
+      handleErrMsg(validInputs.error);
+    }
+    console.log("Valid form:", validInputs);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Trails>
-        <div className=" bg-yellow">
-          <Comment comment="Happy to collaborate!" />
-        </div>
-        <Input label="Name" placeholder="Jane Smith" id="name" name="name" />
-        <Input
-          label="Email"
-          placeholder="JaneSmith@gmail.com"
-          id="email"
-          name="email"
-        />
-        <Input
-          label="Phone Number (optional)"
-          placeholder="(661)-099-9090"
-          id="phoneNumber"
-          name="phoneNumber"
-        />
-        <TextArea
-          label="Let's chat"
-          placeHolder="Hi, I have an app idea I need to come to life!"
-          id="msg"
-          name="msg"
-        />
-        <div className=" mt-24 flex justify-end pr-3">
-          <button
-            onClick={() => handleSubmit}
-            type="submit"
-            className="inline-flex items-center gap-x-1.5 rounded-md bg-iGrey px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Send
-            <CheckCircleIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
-          </button>
-        </div>
-      </Trails>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <Trails>
+          <div className=" bg-yellow">
+            <Comment comment="Happy to collaborate!" />
+          </div>
+          <Input label="Name*" placeholder="Jane Smith" id="name" name="name" />
+          <Input
+            label="Email*"
+            placeholder="JaneSmith@gmail.com"
+            id="email"
+            name="email"
+          />
+          <Input
+            label="Phone Number (optional)"
+            placeholder="(661)-099-9090"
+            id="phoneNumber"
+            name="phoneNumber"
+          />
+          <TextArea
+            label="Let's chat"
+            placeHolder="Hi, I have an app idea I need to come to life!"
+            id="msg"
+            name="msg"
+          />
+          <div className=" mt-24 flex justify-end pr-3">
+            <button
+              onClick={() => handleSubmit}
+              type="submit"
+              className="inline-flex items-center gap-x-1.5 rounded-md bg-iGrey px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-iBlue"
+            >
+              Send
+              <CheckCircleIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
+            </button>
+          </div>
+        </Trails>
+        <p className=" text-red px-3">{errMsg}</p>
+      </form>
+    </>
   );
 };
---
+++ src/components/utils/validateContactForm.ts
@@ -0,0 +1,38 @@
+type V = {
+  value: string;
+};
+
+export interface Target extends EventTarget {
+  name: V;
+  email: V;
+  phoneNumber: V;
+  msg: V;
+}
+
+type FormData = {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  msg: string;
+};
+
+const regiEmail = /\S+@\S+\.\S+/;
+
+export const validateContactForm = (
+  data: FormData
+): { valid: boolean; error: string } => {
+  const validEmail = regiEmail.test(data.email);
+  if (!validEmail && data.name.length <= 3) {
+    return { valid: false, error: "Please enter a valid name and email." };
+  }
+  if (!validEmail) {
+    return { valid: false, error: "Invaild email." };
+  }
+  if (data.name.length <= 3) {
+    return { valid: false, error: "Invaild name." };
+  }
+  if (data.msg.length < 10) {
+    return { valid: false, error: "Please enter a message." };
+  }
+  return { valid: true, error: "" };
+};
